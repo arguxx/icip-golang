cdf2071ddfd0fb23490f800e9e9e033b6ab503e2
arguxx <argans@student.telkomuniversity.ac.id>
2023-04-07 23:34:26 +0700

    init

---
+++ main.go
@@ -0,0 +1,21 @@
+// nama packagenya sesuain aja sama nama filenya
+package main
+
+// cara import di golang
+import "fmt"
+
+//  cara nulis function di golang
+func main()  {
+	// setiap pemisah ada baris baru dan ada spasi
+	fmt.Println("kuda lari", "kuda besar", "gagah berani")
+	// kebalikannya
+	fmt.Print("kuda", "besar")
+	fmt.Print("kuda", "kecil")
+	
+}
+
+// cara nulis comment
+/* 
+kalo ini commentnya
+multi line 
+*/
