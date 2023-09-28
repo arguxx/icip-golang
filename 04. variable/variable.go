2c79ae92dd6afa9cb17dd52f47067c2421d280f6
argian raditya <argian.raditya@tabeldata.com>
2023-09-28 14:41:38 +0700

    try variable in golang

---
+++ 04. variable/variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var name string
+
+	name = "Argian Raditya Putra"
+	fmt.Println(name)
+
+	name = "Argian Raditya"
+	fmt.Println(name)
+
+	var age = 123
+	fmt.Println(age)
+
+	home := "Depok"
+	fmt.Println(home)
+
+	home = "bandung"
+	fmt.Println(home)
+
+	var (
+		firstName = "argian"
+		lastName = "Raditya"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+}
