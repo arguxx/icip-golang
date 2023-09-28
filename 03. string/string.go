695448fefcac60b1b5ae3aaa687f5b0694711dbf
argian raditya <argian.raditya@tabeldata.com>
2023-09-28 14:26:09 +0700

    try string, length, index

---
+++ 03. string/string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("argian")
+	fmt.Println(len("argian raditya putra"))
+	fmt.Println("argian raditya"[3])
+}
+
+// pas ngambil huruf di index ke 3 hasilnya berbentuk byte, harus dikonversi lagi
